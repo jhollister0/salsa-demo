643e4134d9b5c039cfef0259384e680f4dfc0e94
jhollister0 <jameshollister@email.arizona.edu>
2021-05-19 10:53:33 -0700

    Update to cooking time
    5 to 10 minutes

--- salsa_verde.m
+++ salsa_verde.m
@@ -3,9 +3,9 @@
 % Get the ingredients
 [tomatillos, jalapenos, onion, garlic, cilantro] = buy('ingredients.txt');
 
-% Boil the veggies for 5 minutes
+% Boil the veggies for 10 minutes
 boilMe = [tomatillos, jalapenos, onion, garlic];
-boiledVeggies = boil(boilMe, 5);
+boiledVeggies = boil(boilMe, 10);
 
 % Blend with the cilantro and salt
 salsa = blend(boiledVeggies, cilantro, salt);
